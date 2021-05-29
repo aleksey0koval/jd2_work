4a30ec62ca6c5ebca84babc07959fd5d54a99fb0
Aleksey Kovalchuk <aleksey0koval@gmail.com>
2021-05-29 14:00:08 +0300

    Added lesson Spring Data Rest

---
+++ spring_data_rest/src/main/java/com/akovalchuk/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.akovalchuk.spring.springboot.spring_data_rest.dao;
+
+import com.akovalchuk.spring.springboot.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
